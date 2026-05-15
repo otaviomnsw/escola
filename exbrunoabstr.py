bd6afea178f8552e8a9f383a8c645fa08b9693f0
Otávio <manualswordotaconta@gmail.com>
2026-05-15 14:25:23 -0300

    Update exbrunoabstr.py

--- exbrunoabstr.py
+++ exbrunoabstr.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from math import pi, sqrt
+from math import pi
 
 class Forma(ABC):
     def __init__(self, cor):
@@ -29,17 +29,31 @@ class Quadrado(Forma):
         print(f"O perímetro quadrado é: {self.lado*4}")
 
 class Triangulo(Forma):
-    def __init__(self, cor, lado, base,):
+    def __init__(self, cor, lado1, lado2, base, altura):
         super().__init__(cor)
-        self.lado1 = lado
-        self.altura = lado*sqrt(3)/2
+        self.lado1 = lado1
+        self.lado2 = lado2
         self.base = base
+        self.altura = altura
+        while self.lado1 + self.lado2 < self.base or self.lado1 + self.base < self.lado2 or self.lado2 + self.base < self.lado1:
+            print("Este triângulo é invalido! Por favor digite outras medidas: ")
+            self.lado1 = int(input("Digite o lado 1 :"))
+            self.lado2 = int(input("Digite o lado 2 : "))
+            self.base = int(input("Digite a base: "))
 
     def area(self):
-        print(f"A área do triângulo é: {self.base*self.altura/2:.2f}")
-    
+        if self.lado1 == self.lado2 == self.base:
+            self.areatr = self.lado1**2 * 3**0.5 / 2
+        else:
+            self.areatr = self.altura*self.base/2
+        print(f"A área do triângulo é: {self.areatr}")
+        
     def perimetro(self):
-        print(f"O perímetro do triângulo é: {self.base+self.lado1*2:.2f}")
+        if self.lado1 == self.lado2 == self.base:
+            self.perim = self.lado1*3
+        else:
+            self.perim = self.lado1 + self.lado2 + self.base
+        print(f"O perímetro do triângulo é: {self.perim}")
 
 class Circulo(Forma):
     def __init__(self, cor, raio):
@@ -68,17 +82,17 @@ class Hexagono(Forma):
     def __init__(self, cor, lado):
         super().__init__(cor)
         self.lado = lado
-        self.altura = lado*sqrt(3)/2
+        self.altura = lado*3**0.5/2
 
     def area(self):
-        print(f"A área do hexágono é: {self.lado**2*3*sqrt(3)/2:.2f}")
+        print(f"A área do hexágono é: {(self.lado**2)*3*(3**0.5)/2:.2f}")
 
     def perimetro(self):
         print(f"O períemtro do hexágono é: {self.lado*6}")
 
 # obj1 = Forma("Verde")
 qua1 = Quadrado("Branco", 5)
-tri1 = Triangulo("Preto", 3, 4)
+tri1 = Triangulo("Preto", 10, 4, 7, 8)
 cir1 = Circulo("Azul", 4)
 ret1 = Retangulo("Vermelho", 4, 6)
 hex1 = Hexagono("Ciano", 6)
@@ -94,8 +108,8 @@ for i in formas:
 1 - Deu erro, já que não se pode criar um objeto com classe abstrata
 2 - Irá dar erro, já que nesse código não adcionei setter. Usamos ele para deixar mais fácil a alteração do código.
 3 - A cor e o getter da cor.
-4 - O método foi de acordo com a classe, sendo diferente para cada uma.
-5 - Já fiz isso, mas se for considerar um triângulo retângulo, ai precisaria saber a altura através de a² + b² = c², e então fazer a conta.
+4 - O método foi de acordo com a classe, sendo diferente para cada classe específica.
+5 - Feita no código, apenas basta adicionar código extra.
 6 - Este princípio fala sobre como softwares têm de estar abertos para extensão e fechados para modificação, ou seja, deve conseguir 
 adicionar novos comportamentos mas sem alterar o código existente que já funciona. Conseguimos ver com a classe abstrata, em que podemos
 adicionar mais coisas nela ou adicionar novas formas como outras classes que vão herdar de Forma.
